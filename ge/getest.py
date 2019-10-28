9fa10d286d31e16eeb478fddfac0699995cc2a7b
jhover <jhover@transit.cshl.edu>
2019-10-28 14:21:55 -0400

    Update.

---
+++ ge/getest.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+#
+# read in starout format file. 
+# create Series
+# merge Series to dataframe
+#
+
+#  STAR
+# -quantMode GeneCounts 
+# -hseq-count -s yes   /  -s reverse
+# columns
+# geneID       unstranded_counts   1st_stranded  2nd_stranded
+
+# N_unmapped       2674617    2674617    2674617
+# N_multimapping   2828822    2828822    2828822
+# N_noFeature      5598327    24570638    24512243
+# N_ambiguous      3871509    813790    812892
+# ENSG00000223972.5    0    0    0
+# ENSG00000227232.5    302    159    143
+# ENSG00000278267.1    9    7    2
+# ENSG00000243485.4    1    1    0
+# ENSG00000237613.2    0    0    0
+#
+#
+
+
+
+import argparse
+import logging
+
+import pandas as pd
+import numpy as np
+
+def processfile(filename):
+    logging.info("Processing file %s" % filename)
+    f = open(filename)
+    lines = f.readlines()
+    headers = {}
+    data = {}
+    
+    # handle headers    
+    for line in lines[:4]:
+
+        (label, unstrand, strand1, strand2) = [ f.strip() for f in line.split('\t') ]
+        headers[label] = [ unstrand, strand1, strand2 ]
+        
+    # handle values. 
+
+    for line in lines[4:]:
+        (label, unstrand, strand1, strand2) = [ f.strip() for f in line.split('\t') ]
+        data[label] = [unstrand, strand1, strand2 ]
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s')
+    
+    parser = argparse.ArgumentParser()
+      
+    parser.add_argument('-d', '--debug', 
+                        action="store_true", 
+                        dest='debug', 
+                        help='debug logging')
+
+    parser.add_argument('-v', '--verbose', 
+                        action="store_true", 
+                        dest='verbose', 
+                        help='verbose logging')
+
+    parser.add_argument('infiles', 
+                        metavar='infiles', 
+                        type=str, 
+                        nargs='+',
+                        help='a list of single-cell expression files')
+    
+    args= parser.parse_args()
+    
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+    if args.verbose:
+        logging.getLogger().setLevel(logging.INFO)
+    
+    filelist = args.infiles 
+    logging.debug("%d files to process. " % len(filelist))
+    #for fn in filelist:
+    #    processfile(fn)
+
+    processfile(filelist[0])
