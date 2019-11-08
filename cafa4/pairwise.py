4696c6e59fa8350a0b9c8f6b1d3c9147b5e3d21e
jhover <jhover@transit.cshl.edu>
2019-11-08 15:07:14 -0500

    Update.

--- cafa4/pairwise.py
+++ cafa4/pairwise.py
@@ -9,17 +9,31 @@
 #             -asequence 001R_FRG3G.fasta 
 #             -bsequence 002L_FRG3G.fasta 
 #             -outfile 001R_FRG3Gx002L_FRG3G.water
+
+#  time needle -gapopen 10.0 
+#              -gapextend 0.5   
+#              -asequence 001R_FRG3G.fasta 
+#              -bsequence 002L_FRG3G.fasta 
+#              -outfile 001R_FRG3Gx002L_FRG3G.water
+
+#
+#  Esprit http://www.ijbcb.org/ESPRITPIPE/php/download.php
+#  Won tests of PSA tools. ICBR Interdisciplinary Centor for Biotechnology Research
+#    University of Florida. 
+#  ??
+# https://www.majordifferences.com/2016/05/difference-between-global-and-local.html
+#
 #
 #
 
 import argparse
 import logging
 import os
+import subprocess
 import sys
 import traceback
 
-
-class ProcessingRun(object):
+class PairwiseRun(object):
     
     def __init__(self, filelist, workdir ):
         self.log = logging.getLogger()
@@ -28,10 +42,29 @@ class ProcessingRun(object):
         if not os.path.exists(self.workdir):
             os.mkdir(self.workdir)
             self.log.info("Created workdir %s" % self.workdir)
+        self.log.debug("Created PairwiseRun workdir=%s" % self.workdir)
 
 
     def runwater(self, f1, f2):
-        self.log.debug("comparing file %s to file %s" % ( f1, f2))
+        self.log.debug("water: comparing file %s to file %s" % ( f1, f2))
+        outfile = "%s/%sx%s.water" % (self.workdir, f1, f2)
+        self.log.debug("outfile=%s" % outfile)
+        cmdlist = ['time water']
+        cmdlist.append( '-gapopen 10.0' )
+        cmdlist.append('-gapextend 0.5' )
+        cmdlist.append('-asequence %s' % f1 )
+        cmdlist.append('-bsequence %s' % f2 )
+        cmdlist.append('-outfile %s' % outfile )
+        self.log.debug("cmdlist=%s" % cmdlist)
+        cmd = ' '.join(cmdlist).strip()
+        self.log.debug("command is '%s'" % cmd)
+        self.log.info("Running %s against %s" % (f1, f2) )
+        cp = subprocess.run(cmd, check=True, shell=True)
+        self.log.debug("Completed generating %s" % outfile)
+        
+    
+    def runneedle(self, f1, f2):
+        self.log.debug("needle: comparing file %s to file %s" % ( f1, f2))    
 
 
     def handlefiles(self):
@@ -45,9 +78,8 @@ class ProcessingRun(object):
                 f2 = filelist[j]
                 self.log.debug("comparing file %s to file %s" % ( f1, f2))
                 self.runwater(f1, f2)
-                       
-        
-        
+                     
+            
         #for filename in self.filelist:
         #    filename = os.path.relpath(filename)
         #                
@@ -88,7 +120,7 @@ if __name__ == '__main__':
     parser.add_argument('-w', '--workdir', 
                         action="store", 
                         dest='workdir', 
-                        default='~/work/cafa4-play/seqwork',
+                        default='~/work/cafa4-play/seqout',
                         help='run-specific workdir [~/work/cafa4-play/seqout]')
                    
     args= parser.parse_args()
@@ -99,5 +131,5 @@ if __name__ == '__main__':
         logging.getLogger().setLevel(logging.INFO)
     
     filelist = args.infiles 
-    run = ProcessingRun(filelist, args.workdir)
+    run = PairwiseRun(filelist, args.workdir)
     run.handlefiles()
