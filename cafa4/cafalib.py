ed918a6cfdf5cabe139082f8685955496ecbf227
jhover <jhover@library1.cshl.edu>
2019-12-09 16:13:00 -0500

    Update.

--- cafa4/cafalib.py
+++ cafa4/cafalib.py
@@ -56,11 +56,14 @@ class CAFA4Run(object):
         self.log.debug(phm)
         df = phm.execute()
         df.drop(columns=['target','bias','prot_spec'], inplace=True)
-        self.log.info(str(df))
+        self.log.info("\n%s" % str(df))
         df.to_csv("%s/phmmer.csv" % self.outdir)
-      
-
-
+        
+        ortho = Orthologs(self.config)
+        self.log.debug(ortho)
+        df = ortho.execute(df)
+        self.log.info("\n%s" % str(df))
+        df.to_csv("%s/ortho.csv" % self.outdir)
         self.log.info("Ending run...")
 
 
@@ -138,7 +141,7 @@ class Phmmer(object):
     def read_phmmer_table(self, filename):
         df = pd.read_table(filename, 
                          names=['target','t-acc','query','q-acc',
-                                'e-value', 'score', 'bias', 'e-value-dom','score-dom', 'bias-dom', 
+                                'evalue', 'score', 'bias', 'e-value-dom','score-dom', 'bias-dom', 
                                 'exp', 'reg', 'clu',  'ov', 'env', 'dom', 'rep', 'inc', 'description'],
                          skip_blank_lines=True,
                          comment='#',
@@ -164,13 +167,20 @@ class Orthologs(object):
     Output: Pandas DataFrame
     '''
 
-    def __init__(self):
+    def __init__(self, config):
         '''
         
         '''
         self.log = logging.getLogger()
+        self.config = config
+        self.outdir = os.path.expanduser( config.get('global','outdir') )
         self.uniprot = uniprot.UniProt()
-         
+
+    def __repr__(self):
+        s = "Orthologs: uniprot"
+        for atr in ['outdir']:
+            s += " %s=%s" % (atr, self.__getattribute__(atr))
+        return s            
 
         
     def execute(self, dataframe):
@@ -178,18 +188,64 @@ class Orthologs(object):
         for each row of dataframe, look up ortholog in uniprot and for each GO code
         add a new row with gene, goterm, gocategory
         
-        return result dataframe
+        iterate input df fully, putting new info in new df. 
+        merge old + new df, return resulting dataframe
         
         '''
-        pass
-    
+        self.log.info("Looking up each ortholog")
+        entries = dataframe['tacc'].unique().tolist()
+        self.log.debug("Querying uniprot for %d unique entries" % len(entries))
+        udf = self.uniprot.get_df(entries)
+        self.log.debug(udf)
+        udf.to_csv("%s/uniprot.csv" % self.outdir)
+        udfslim = udf[['Entry','Gene names','Gene ontology IDs']] 
+        # df.tacc corresponds to udf.Entry  ...
+        
+        newrowdict = {} 
+        ix = 0 
+        for row in udfslim.itertuples():
+            (entry, gene_names, golist) = row[1:] 
+            #print("gene_names is %s" % gene_names) 
+            try: 
+                namestr = gene_names[0] 
+                gene = namestr.split()[0] 
+            except: 
+                gene = '' 
+            for goterm in golist: 
+                #print("creating new row: %s : %s %s %s" % (ix, entry, gene, goterm)) 
+                newrow = [ entry, gene, goterm ] 
+                newrowdict[ix] = newrow 
+                ix += 1       
+            #print("entry is %s" % entry) 
+            #print("gene_names are %s" % gene_names) 
+            #print("gene is %s" % gene) 
+            #print("golist is %s" % golist)     
+            #print("newrowdict is %s" % newrowdict) 
+        
+        godf = pd.DataFrame.from_dict(newrowdict, orient='index', columns=['entry','gene','goterm']) 
+        #print(newdf)
+        
+        newdfdict= {}
+        ix = 0
+        for row in dataframe.itertuples():
+            (query, evalue, score, db, tacc, protein, species) = row[1:]
+            gomatch = godf[ godf.entry == tacc ]
+            for gr in gomatch.itertuples():
+                (entry, gene, goterm) = gr[1:]
+                newrow = [query, evalue, score, db, tacc , protein, species, gene, goterm ]
+                newdfdict[ix] = newrow
+                ix += 1
+        newdf = pd.DataFrame.from_dict(newdfdict, orient='index', columns = ['query', 'evalue', 'score', 'db', 'tacc' , 
+                                                                             'protein', 'species', 'gene', 'goterm'])
+        self.log.debug("\n%s" % str(newdf))
+        return newdf
+            
     
     def _query_uniprot(self, tacc):
         '''
         query http://www.uniprot.org/uniprot/<tacc>.txt and parse for gene, goterm, gocategory. 
         
         u.search("id:P35213")
-        
         
         get_df(self, entries, nChunk=100, organism=None):
             entries=['id:P35213'] 
