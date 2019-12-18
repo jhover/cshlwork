ebbfe92f202642f0312692eafbf1a37898ccb714
jhover <jhover@library1.cshl.edu>
2019-12-18 16:33:29 -0500

    Update.

--- cafa4/uniprot.py
+++ cafa4/uniprot.py
@@ -7,6 +7,7 @@ import argparse
 from configparser import ConfigParser
 import logging
 import os
+import pprint
 
 import pandas as pd
 import pdpipe as pdp
@@ -14,13 +15,101 @@ import numpy as np
 import pyarrow as pa
 import pyarrow.parquet as pq
 import subprocess
+import sys
+import traceback
 
 from bioservices.uniprot import UniProt   # to query online REST interface
 from Bio import SeqIO   # to parse uniprot.dat
 
+
 #
 #  Possibly compare  https://pypi.org/project/PyUniProt/
 #
+
+class UniProtRecord(object):
+    
+    def __init__(self, record):
+        '''
+        
+        ID: Q6GZW9
+        Name: 006R_FRG3G
+        Description: RecName: Full=Uncharacterized protein 006R;
+        Database cross-references: EMBL:AY548484, RefSeq:YP_031584.1, SMR:Q6GZW9, GeneID:2947778, KEGG:vg:2947778, Proteomes:UP000008770
+        Number of features: 1
+        /accessions=['Q6GZW9']
+        
+        
+        record:
+            id:
+            name:
+            description: 
+            annotations: -> dict
+            dbxrefs: -> list     
+            features: -> SeqFeature
+
+        'letter_annotations', 
+        'lower', 
+        'name', 'reverse_complement', 'seq', 'translate', 'upper'
+        
+        E.g 
+        annotations -> dict
+            'accessions': ['Q6GZW9'], 
+            'protein_existence': 4, 
+            'date': '28-JUN-2011', 
+            'sequence_version': 1, 
+            'date_last_sequence_update': '19-JUL-2004', 
+            'date_last_annotation_update': '05-JUN-2019', 
+            'entry_version': 28, 
+            'gene_name': 'ORFNames=FV3-006R;', 
+            'organism': 'Frog virus 3 (isolate Goorha) (FV-3)', 
+            'taxonomy': ['Viruses', 'Iridoviridae', 'Alphairidovirinae', 'Ranavirus'], 
+            'ncbi_taxid': ['654924'], 
+            'organism_host': ['Ambystoma (mole salamanders)', 
+                               'Dryophytes versicolor (chameleon treefrog)', 
+                               'Lithobates pipiens (Northern leopard frog) (Rana pipiens)', 
+                               'Notophthalmus viridescens (Eastern newt) (Triturus viridescens)',
+                               'Rana sylvatica (Wood frog)'], 
+                               'host_ncbi_taxid': ['8295', 
+                                                    '30343', 
+                                                    '8404', 
+                                                    '8316', 
+                                                    '45438'], 
+                                'references': --> Reference 
+        
+        dbxrefs -> list
+            ['EMBL:AY548484', 'RefSeq:YP_031579.1', 'SwissPalm:Q6GZX4', 'GeneID:2947773', 
+            'KEGG:vg:2947773', 'Proteomes:UP000008770', 
+            'GO:GO:0046782', 
+            'InterPro:IPR007031', 'Pfam:PF04947']
+    
+        [Reference(title='Comparative genomic analyses of frog virus 3, type species of the genus Ranavirus (family Iridoviridae).',
+                     ...)], 
+                  'keywords': ['Complete proteome', 
+                              'Reference proteome']
+        '''
+        self.proteinid = record.id
+        self.protein = record.name
+        self.goterms = []
+        for xf in record.dbxrefs:
+            if xf.startswith("GO:"):
+                gt = xf[3:]
+                self.goterms.append(gt)
+        self.accessions = record.annotations['accessions']
+        self.taxonid = record.annotations['ncbi_taxid'][0]
+        #self.species = 
+        #if 'accessions' in record.annotations.keys():
+        #    self.accessions = record.annotations['accessions']
+            
+            
+            
+    def __repr__(self):
+        REPR_ATTRS=['proteinid','protein','accessions', 'taxonid','goterms']
+        s="UniProtRecord: "
+        for atr in REPR_ATTRS:
+            s+="%s=%s " % (atr, 
+                           self.__getattribute__(atr))
+        return s
+
 
 class UniProtGOPlugin(object):
     '''
@@ -34,6 +123,7 @@ class UniProtGOPlugin(object):
         self.uniprot = UniProt()
         self.outdir = os.path.expanduser( config.get('global','outdir') )
         self.sprotdatfile = os.path.expanduser( config.get('goplugin','sprotdatfile') )
+        self.speciesmap = os.path.expanduser( config.get('global','species_mapfile'))
 
 
     def get_df(self, dataframe):
@@ -114,10 +204,42 @@ class UniProtGOPlugin(object):
 
 
     def get_swissprot_df(self):
-        from Bio import SeqIO
+        '''
+    
+        Dataframe:
+        proteinid   name   
+
+cf... 
+             cafaid         evalue  score  bias  db proteinid protein species cafaprot cafaspec       gene      goterm  probest goaspect
+0     T100900000001  1.100000e-156  523.6   8.5  sp    Q9CQV8   1433B   MOUSE    1433B    MOUSE      Ywhab  GO:0005634      1.0       cc
+    
+        '''
+        rlist = self._dat2upr()
+        for r in rlist:
+            print(r)
+        #self.id = record.id
+        #self.name = record.name
+            
+        
+
+    
+    def _dat2upr(self):
+        self.log.debug("opening swissprot dat file %s" % self.sprotdatfile)
         rgen = SeqIO.parse(self.sprotdatfile,"swiss")
+        i = 0
+        uprlist = []
+
         for record in rgen:
-            pass
+            upr = UniProtRecord(record)
+            uprlist.append(upr)
+            print(record)
+            i += 1
+            if i >= 10:
+                break
+        self.log.debug("parsed dat file of %d records" % len(uprlist))
+        return uprlist
+
+
 
 
     def _parsefile(self, filehandle):
@@ -144,7 +266,72 @@ class UniProtGOPlugin(object):
             traceback.print_exc(file=sys.stdout)                
         
         self.log.debug("Parsed file with %d terms" % keyid )
-  
+
+    def _make_species_map(self):
+        '''
+        Parses uniport speclist.txt    https://www.uniprot.org/docs/speclist.txt
+        to local .CSV
+        
+        taxonid   species   lineanname       commonname
+        72259      ABANI    Abaeis nicippe   Sleepy orange butterfly
+                                             
+        OXYMO E  475340: N=Oxytenis modestia
+                         C=Costa Rica leaf moth
+                         S=Dead-leaf moth
+        
+        
+        '''
+        listfile = self.speciesmap
+        self.log.debug("Opening species map file %s" % listfile)
+        try:
+            fh = open(listfile, 'r')
+        except FileNotFoundError:
+            self.log.error("No such file %s" % filename)                
+       
+        species = None
+        kingdom = None
+        taxonid = None
+        lineanname = None
+        commonname = None
+        
+        columnnames = ['species','kingdom','taxonid','lineanname','commonname']
+        datalist = []
+        # list of tuples
+              
+        try:
+            for line in fh:
+                #self.log.debug("handling line %s" % line)
+                if 'N=' in line and not line.startswith('Code')  :
+                    #self.log.debug("handling N= line. taxonid is %s" % taxonid)
+                    if species is not None:
+                        tup = (species, kingdom, taxonid, lineanname, commonname)
+                        #self.log.debug("Adding tuple: %s" % str(tup))
+                        datalist.append( tup )
+                        # reset all varaiables
+                        species = kingdom = taxonid = lineanname = commonname = None
+                    species = line[:5]
+                    kingdom = line[6]
+                    taxonid = line[7:15].strip()
+                    lineanname = line[19:].strip()
+                    #self.log.debug("handling N= line. taxonid is %s" % taxonid)         
+                elif 'C=' in line :
+                    commonname = line[19:].strip()
+                elif 'S=' in line :
+                     pass
+        except Exception as e:
+            traceback.print_exc(file=sys.stdout)                
+        finally:
+            fh.close()
+        
+        self.log.debug("Parsed file with %d terms" % len(datalist) )
+    
+        df = pd.DataFrame(datalist, columns = columnnames)
+        outfile = "%s/speclist.csv" % self.outdir
+        self.log.debug("Writing dataframe to %s" % outfile )
+        df.to_csv(outfile )
+        print(str(df))
+        return df
+
         
 
 def test_uniprot(config):
@@ -156,6 +343,11 @@ def test_uniprot(config):
 def test_datparse(config):
     upg = UniProtGOPlugin(config)
     upg.get_swissprot_df()
+
+def test_speciesmap(config):
+    upg = UniProtGOPlugin(config)
+    upg._make_species_map()
+
     
 
 
@@ -188,8 +380,8 @@ if __name__ == '__main__':
     cp.read(args.conffile)
 
     # test_uniprot(config)
-    test_datparse(cp)
-  
+    #test_datparse(cp)
+    test_speciesmap(cp)
 
     
     
