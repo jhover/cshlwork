cf6fb883e3a78f8e907be7068bba6e12ea45bda0
jhover <jhover@transit.cshl.edu>
2020-02-12 16:55:29 -0500

    Update.

--- scripts/summarizeeval.py
+++ scripts/summarizeeval.py
@@ -15,25 +15,29 @@ from fastcafa.fastcafa import *
 if not len(sys.argv) > 1:
     print("need input file arg")
     sys.exit()
+f1maxes = []
     
-infile = sys.argv[1]
+infiles = sys.argv[1:]
+for infile in infiles:
+    basename = os.path.basename(infile)
+    # print(f"{infile}")
+    try:
+        df = pd.read_csv(infile, index_col=0, comment="#")
+        
+        totaltrue = len(df[df.correct == True])
+        totalnum = df.shape[0] 
+        accuracy = totaltrue / totalnum
+        numcids = len(df.cid.unique() )
+        f1max = df.groupby('cid')['f1max'].max().mean()  
+        f1maxes.append(f1max)
+                
+        #print(f"num cafaids: {numcids}")
+        print(f"{basename}\tf1max: {f1max}")
+        #print(f"accuracy: {accuracy}")
+    except FileNotFoundError:
+        print(f"no such file {infile}")
 
-basename = os.path.basename(infile)
-# print(f"{infile}")
-try:
-    df = pd.read_csv(infile, index_col=0, comment="#")
-    #print(f"{df}")
-except FileNotFoundError:
-    print(f"no such file {infile}")
-    sys.exit()
-    
-totaltrue = len(df[df.correct == True])
-totalnum = df.shape[0] 
-accuracy = totaltrue / totalnum
-numcids = len(df.cid.unique() )
-meanf1max = df.groupby('cid')['f1max'].max().mean()  
+ar = np.array(f1maxes)
+meanf1max = np.mean(ar)
 
-
-#print(f"num cafaids: {numcids}")
-print(f"{basename} meanf1max: {meanf1max}")
-#print(f"accuracy: {accuracy}")
+print(f">meanf1max(allfiles):\t\t{meanf1max}")
