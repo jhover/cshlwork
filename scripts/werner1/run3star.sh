3bf34c2f680976cebb752999df16484e30b40536
jhover <jhover@143.48.48.58>
2020-11-06 14:03:08 -0500

    Update.

--- scripts/werner1/run3star.sh
+++ scripts/werner1/run3star.sh
@@ -2,35 +2,42 @@
 #   Usage: runstar <setup> <genomedir> <infile1> <infile2> <outdir> <filebase>
 #
 #   args =  $(setup) 
+#           $(basefile)
 #           $(genomedir) 
-#           $(outdir)/$(filebase).end1.fq  
-#           $(outdir)/$(filebase).end2.fq  
-#           $(outdir) 
-#           $(filebase)
+#           $(outdir)
+#						$(outdir)/$(filebase).end1.fq  
+#           			$(outdir)/$(filebase).end2.fq  
+#           			$(outdir) 
+#           			X $(filebase)
 #   request_cpus = 16
 #   request_memory = 51200
 #
+# job name
+#$ -N run3star
+#
+# job indexes for array all jobs, but only run 10 at a time for disk quota 
+#$ -t 1-4 -tc 2
+#
+# processes per job
+#$ -pe threads 16 
+#
+#$ -wd /grid/gillis/data/hover/work/werner1/
+#
+# Per-thread memory request. 
+#$ -l m_mem_free=3G
+# 
+COMMON=~/git/cshl-work/scripts/werner1/common.sh 
+
+NUMTHR=16
+
 
 echo "*********START*************************"
 date
-echo "*********NODE*************************"
-hostname -f
-WD=`pwd`
-echo "Working Directory: $WD"
-cat /etc/redhat-release
-CV=`condor_version | tr -d "\n"`
-echo "Condor Version: $CV"
-NPROC=`cat /proc/cpuinfo  | grep processor | wc -l`
-echo "Processors: $NPROC "
-KMEM=`cat /proc/meminfo  | grep MemTotal | awk '{print $2}'`
-MBMEM=`expr $KMEM / 1000`
-echo "Memory MB: $MBMEM"
-
-
+. $COMMON
 
 echo "*********JOB*************************"
 echo "Args are $@"
-if [ $# -ne 6 ]; then
+if [ $# -ne 4 ]; then
     echo "Incorrect number of arguments."
     "Usage: runstar <setup> <genomedir> <infile1> <infile2> <outdir> <filebase>"
     exit 1
@@ -40,9 +47,23 @@ echo "Running setup from $1"
 . $1
 echo "PATH=$PATH"
 
+basefile=$2
+# $SGE_TASK_ID
+filebase=`head -$SGE_TASK_ID $2 | tail -1 `
+echo "Filebase is $filebase"
+
+infile1="$4/$filebase.end1.fq "
+infile2="$4/$filebase.end2.fq"
+
+genomdir=$3
+
 echo "Running job..."
-echo STAR  --genomeDir $2 --readFilesIn $3 $4 --runThreadN 20 --twopassMode Basic --twopass1readsN -1 --outSAMtype BAM Unsorted --quantMode GeneCounts  
-time STAR  --genomeDir $2 --readFilesIn $3 $4 --runThreadN 20 --twopassMode Basic --twopass1readsN -1 --outSAMtype BAM Unsorted  --quantMode GeneCounts
+
+mkdir $filebase
+cd $filebase
+
+echo STAR  --genomeDir $genomedir --readFilesIn $infile1 $infile2 --runThreadN $NUMTHR --twopassMode Basic --twopass1readsN -1 --outSAMtype BAM Unsorted --quantMode GeneCounts  
+time STAR  --genomeDir $genomedir --readFilesIn $infile1 $infile2 --runThreadN $NUMTHR --twopassMode Basic --twopass1readsN -1 --outSAMtype BAM Unsorted  --quantMode GeneCounts
 RET=$?
 if [ $RET -ne 0 ] ; then
     exit $RET
@@ -51,13 +72,14 @@ echo "Job command Return code was $RET"
 
 echo "********STAGEOUT**********************"
 echo "Contents of working dir:"
-ls -alh $WD
+ls -alh
 KEEPFILES="Aligned.out.bam Log.out Log.final.out Log.progress.out ReadsPerGene.out.tab SJ.out.tab"
 for f in $KEEPFILES; do
-    echo mv -v $WD/$f $5/$6.$f
-    mv -v $WD/$f $5/$6.$f
+    echo mv -v $f ../$basefile.$f
+    mv -v $f ../$basefile.$f
 done
+
+cd ..
 
 echo "*********END***************************"
 exit $RET
-
