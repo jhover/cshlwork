f552b5dd385880e93c7798eaeaa666ad2d94f7aa
jhover <jhover@transit.cshl.edu>
2021-02-01 10:31:15 -0500

    Update.

---
+++ project/eqtl/softenv-install.sh
@@ -0,0 +1,83 @@
+#
+#
+#
+#
+conda create -n fastqtl
+
+# For chapter 2  in eQTL analysis 
+conda install -c conda-forge -c bioconda r-base r-genomictools
+
+# Python/Pandas based...
+conda install -c bioconda pyranges
+
+#
+#  gr = pr.read_gtf("ensembl.gtf")
+#  # as DataFrame
+#df = gr.df
+#
+#
+
+cd $CONDA_PREFIX
+mkdir -p ./etc/conda/activate.d
+mkdir -p ./etc/conda/deactivate.d
+touch ./etc/conda/activate.d/env_vars.sh
+touch ./etc/conda/deactivate.d/env_vars.sh
+
+
+# Install RMATH
+cd ~/CONDA_PREFIX/ ; mkdir src ; cd src
+wget http://cran.r-project.org/src/base/R-3/R-3.2.0.tar.gz
+tar xzvf R-3.2.0.tar.gz
+cd R-3.2.0
+./configure --prefix=$CONDA_PREFIX --with-x=no
+cd src/nmath/standalone
+
+
+sudo yum install boost-devel gsl-devel
+
+# install fastqtl
+wget http://fastqtl.sourceforge.net/files/FastQTL-2.184.linux.tgz
+tar -xvzf FastQTL-2.184.linux.tgz
+cd FastQTL-2.184
+# edit Makefile to set RMATH to dir established above...
+make cleanall && make
+mkdir $CONDA_PREFIX/bin
+cp bin/* $CONDA_PREFIX/bin/
+
+
+#
+# Only needed when RPM not installable...
+#
+
+#install gsl
+#wget https://mirror.ibcp.fr/pub/gnu/gsl/gsl-latest.tar.gz
+#./configure --prefix=$CONDA_PREFIX
+# make 
+# make install
+
+
+#install boost
+#https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.gz
+#./bootstrap.sh
+#b2
+
+
+# install zlib
+wget http://www.zlib.net/zlib-1.2.11.tar.gz
+ ./configure --prefix=$CONDA_PREFIX
+make
+make install
+
+#rmath?
+# conda install -c r r-essentials         
+#	(1) Download R source code:             wget http://cran.r-project.org/src/base/R-3/R-3.2.0.tar.gz
+#        (2) Unzip R source code:                        tar xzvf R-3.2.0.tar.gz
+#        (3) Go to R source code folder:         cd R-3.2.0
+#        (4) Configure Makefile:                         ./configure
+#        (5) Go to R math library folder:        cd src/nmath/standalone
+#        (6) Compile the code:                           make
+#        (7) Go 2 folder backward:                       cd ../..
+#        (8) Save the current path:                      RMATH=$(pwd)
+wget http://cran.r-project.org/src/base/R-3/R-3.2.0.tar.gz
+
+
---
+++ utils/pdclean.py
@@ -0,0 +1,61 @@
+
+import logging
+import pandas as pd
+
+def convert_columns(dataframe, col_list, lookup_dlists, dest_type, inplace=False ):
+    '''
+    Set values to those looked up from dict and set to dtype. 
+    
+    @param Dataframe to clean. 
+    @param List of one or more columns. 
+    @param List of value lookup dictionaries. 
+    @param Destination type as *string* to ensure all are. 
+    
+    @return altered dataframe
+        
+    '''
+    czip = zip (col_list, lookup_dlists)
+    for (col, lookup)  in czip:
+        def fixfunc(row):
+            try:
+                v = row[col]
+                if isinstance(v, str):
+                    v = v.strip()
+                return lookup[row[col]]
+            except:
+                # if value is not handled, just return existing value. 
+                # makes function idempotent. 
+                return row[col]
+            
+        dataframe[col] = dataframe.apply(lambda row: fixfunc(row), axis=1)
+        dataframe[col] = dataframe[col].astype(dest_type)
+    return dataframe
+
+
+def cast_columns(dataframe, col_list, dest_type, inplace=False ):
+    '''
+    Cast columns in col_list to selected type, setting
+    any values un-convertable to NaNs.     
+    @param Dataframe
+    @param list of columns to be case.
+    @param destination type as string ( 'int32' '
+    
+    '''
+    for col in col_list:
+        def fixfunc(row):
+            try:
+                out = pd.to_numeric(row[col])
+                return row[col]
+            except:
+                # if value is not handled, just return existing value. 
+                # makes function idempotent. 
+                return pd.NA
+            
+        dataframe[col] = dataframe.apply(lambda row: fixfunc(row), axis=1)
+        dataframe[col] = dataframe[col].astype(dest_type)
+    return dataframe
+    
+    
+            
+    
+    
