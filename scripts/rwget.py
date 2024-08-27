465c706102c2a26bef51ccaecd04abb32f6a6435
hover <hover@localhost>
2024-08-27 17:48:04 -0400

    Update.

---
+++ scripts/rwget.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python
+#
+#   wrap wget and provide cleaner  rwget  <src> <dst> 
+#   to make it more like scp, cp, or mv
+#
+import argparse
+import os
+import sys
+import time
+import logging
+import subprocess
+from urllib.parse import urlparse
+
+import datetime as dt
+
+from configparser import ConfigParser
+
+gitpath = os.path.expanduser("~/git/cshlwork")
+sys.path.append(gitpath)
+
+class NonZeroReturnException(Exception):
+    """
+    Thrown when a command has non-zero return code. 
+    """
+
+
+def run_command(cmd):
+    """
+    cmd should be standard list of tokens...  ['cmd','arg1','arg2'] with cmd on shell PATH.
+    @return 
+    
+    """
+    cmdstr = " ".join(cmd)
+    logging.debug(f"command: {cmdstr} running...")
+    start = dt.datetime.now()
+    cp = subprocess.run( cmd, 
+                    text=True, 
+                    stdout=subprocess.PIPE, 
+                    stderr=subprocess.STDOUT
+                    )
+    end = dt.datetime.now()
+    elapsed =  end - start
+    logging.debug(f"ran cmd='{cmdstr}' return={cp.returncode} {elapsed.seconds} seconds.")
+    
+    if cp.stderr is not None:
+        logging.warn(f"got stderr: {cp.stderr}")
+    if cp.stdout is not None:
+        logging.debug(f"got stdout: {cp.stdout}")   
+    if str(cp.returncode) == '0':
+        #logging.debug(f'successfully ran {cmdstr}')
+        logging.debug(f'got rc={cp.returncode} command= {cmdstr}')
+    else:
+        logging.warn(f'got rc={cp.returncode} command= {cmdstr}')
+       
+        #raise NonZeroReturnException(f'For cmd {cmdstr}')
+    return cp
+
+def run_command_shell(cmd):
+    """
+    maybe subprocess.run(" ".join(cmd), shell=True)
+    cmd should be standard list of tokens...  ['cmd','arg1','arg2'] with cmd on shell PATH.
+    
+    """
+    cmdstr = " ".join(cmd)
+    logging.debug(f"running command: {cmdstr} ")
+    start = dt.datetime.now()
+    cp = subprocess.run(" ".join(cmd), 
+                    shell=True, 
+                    stdout=subprocess.PIPE, 
+                    stderr=subprocess.STDOUT)
+
+    end = dt.datetime.now()
+    elapsed =  end - start
+    logging.debug(f"ran cmd='{cmdstr}' return={cp.returncode} {elapsed.seconds} seconds.")
+    
+    if cp.stderr is not None:
+        logging.warn(f"got stderr: {cp.stderr}")
+        pass
+    if cp.stdout is not None:
+        #logging.debug(f"got stdout: {cp.stdout}")
+        pass
+    if str(cp.returncode) == '0':
+        #logging.debug(f'successfully ran {cmdstr}')
+        logging.debug(f'got rc={cp.returncode} command= {cmdstr}')
+    else:
+        logging.warn(f'got rc={cp.returncode} command= {cmdstr}')
+        raise NonZeroReturnException(f'For cmd {cmdstr}')
+    return cp
+
+
+  
+def handle_urlroot(urlroot, dest):
+    logging.debug(f'downloading {urlroot} to {dest}')
+    o = urlparse(urlroot, scheme='', allow_fragments=True)
+    scheme = o.scheme
+    hostname = o.hostname
+    port = o.port
+    urlpath = o.path
+    logging.debug(f'scheme={scheme} host={hostname} path={urlpath}')
+    
+    sleeptime = 15
+    keep_going = True    
+    progress_score = 0
+    runcount = 0
+    min_run = 3  # run at least 3 times to be sure.
+    
+    pfields = urlpath.split('/')
+    pfields = [x for x in pfields if x != '']
+    n_cut = len(pfields) 
+    topdir = pfields[-1]
+    logging.debug(f'topdir={topdir} n_cut={n_cut} min_run={min_run} sleeptime={sleeptime}')
+    
+    while keep_going:
+        runcount += 1
+        logging.info(f'[run {runcount}] running wget to {dest}')
+        
+        copy_cmd = ['wget',
+               '--recursive',
+               '--continue',
+               f'--cut-dirs={n_cut}',  
+               '--no-parent',
+               '-l', '10',
+               '-w', '0.5',   
+               '--no-host-directories',
+               '-R', 'index.html*',
+               f'--directory-prefix={dest}/{topdir}', 
+               urlroot ]
+
+        try:
+            logging.info(f'running wget. run number: {runcount} ')
+            logging.debug(f"command: {' '.join(copy_cmd)}")
+            proc = subprocess.Popen(copy_cmd,    
+                                    stdout=subprocess.DEVNULL,
+                                    stderr=subprocess.DEVNULL,
+                                    close_fds=True)                                    
+        except NonZeroReturnException as nzre:
+            logging.error(f'problem with {infile}')
+            logging.error(traceback.format_exc(None))        
+
+        process_running = True
+        while process_running:
+            logging.debug(f'sleeping {sleeptime} seconds... ')
+            time.sleep(sleeptime)                  
+            rc = proc.poll()
+            if rc is not None:
+                logging.info(f'process has exitted rc={rc} ')
+                old_progress_score = progress_score
+                progress_score = score_dest(f'{dest}/{topdir}')
+                if old_progress_score < progress_score:
+                    logging.info(f'{old_progress_score} < {progress_score}')
+                else:
+                    logging.info(f'{old_progress_score} == {progress_score}. Stop.')
+                    keep_going = False
+                process_running = False
+            else:
+                logging.info(f'process still running.')
+                   
+
+
+def score_dest(destroot):
+    size_bytes = space_check(destroot)
+    file_count = filecount_check(destroot)
+    dest_score = size_bytes * file_count
+    logging.debug(f'got dest_score={dest_score} for {file_count} entries consuming {size_bytes} bytes')
+    return dest_score
+    
+
+def space_check(destroot):
+    spacecheck_cmd = ['du',
+       '-s', destroot 
+       ]
+    try:
+        scp = run_command(spacecheck_cmd)
+        size_bytes = int(scp.stdout.split('\t')[0])
+        return size_bytes
+        
+    except NonZeroReturnException as nzre:
+        logging.error(f'problem with {infile}')
+        logging.error(traceback.format_exc(None))        
+        raise 
+
+def filecount_check(destroot):    
+    filecount_cmd = ['find',
+       '-L', destroot 
+       ]
+    try:
+        fcp = run_command(filecount_cmd)
+        file_count = len(fcp.stdout.split('\n'))
+        return file_count
+    except NonZeroReturnException as nzre:
+        logging.error(f'problem with {infile}')
+        logging.error(traceback.format_exc(None))        
+        raise 
+
+
+
+def wrap_command(cmd):
+    
+    try:
+        run_command(cmd)
+    except NonZeroReturnException as nzre:
+        logging.error(f'problem with {infile}')
+        logging.error(traceback.format_exc(None))        
+        raise 
+
+       
+
+def handle_urlfile(urlfile, dest):
+    logging.debug(f'downloading urls in  {urlfile} to {dest}')
+
+
+if __name__ == '__main__':
+    FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(filename)s:%(lineno)d %(name)s.%(funcName)s(): %(message)s'
+    logging.basicConfig(format=FORMAT)
+    logging.getLogger().setLevel(logging.WARN)
+    
+    parser = argparse.ArgumentParser()
+      
+    parser.add_argument('-d', '--debug', 
+                        action="store_true", 
+                        dest='debug', 
+                        help='debug logging')
+
+    parser.add_argument('-v', '--verbose', 
+                        action="store_true", 
+                        dest='verbose', 
+                        help='verbose logging')
+
+    parser.add_argument('-L','--logfile', 
+                    metavar='logfile',
+                    required=False,
+                    default=None, 
+                    type=str, 
+                    help='Logfile for subprocess.')
+
+
+    parser.add_argument('-u','--urlfile', 
+                        metavar='urlfile',
+                        required=False,
+                        default=None,
+                        type=str, 
+                        help='Text file with one URL per line.  ')
+
+   
+    parser.add_argument('argone' ,
+                        metavar='argone', 
+                        type=str,
+                        default=None, 
+                        nargs='?',
+                        help='root of URL to copy')
+
+    parser.add_argument('argtwo' ,
+                        metavar='argtwo', 
+                        type=str,
+                        default=None, 
+                        nargs='?',
+                        help='destination path ')
+       
+    args= parser.parse_args()
+    
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+    if args.verbose:
+        logging.getLogger().setLevel(logging.INFO)   
+
+    if args.logfile is not None:
+        log = logging.getLogger()
+        FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s'
+        formatter = logging.Formatter(FORMAT)
+        logStream = logging.FileHandler(filename=args.logfile)
+        logStream.setFormatter(formatter)
+        log.addHandler(logStream)
+    
+    logging.debug(f'urlfile={args.urlfile} urlroot={args.argone} dest={args.argtwo}')
+    
+    if args.argtwo is None:
+        args.argtwo = './'
+    
+    if args.urlfile is not None:
+        handle_urlfile(args.urlfile, args.argone)
+    else:
+        handle_urlroot(args.argone, args.argtwo)
+        
+
+
